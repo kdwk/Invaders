ddeecefe30348fbbaec1982b2fd4e9f480159e24
Kdwk <kdwkleung@protonmail.com>
2023-01-13 22:43:44 +0800

    Finished code for rendering frames.

--- src/frame.rs
+++ src/frame.rs
@@ -7,11 +7,11 @@ pub fn new_frame() -> Frame {
     let mut frame = Vec::with_capacity(NUM_COLS);
     
     for _ in 0..NUM_COLS {
-        // let mut col = Vec::with_capacity(NUM_ROWS);
-        // for _ in 0..NUM_ROWS{
-        //     col.push(" ");
-        // }
-        // cols.push(col);
+        let mut col = Vec::with_capacity(NUM_ROWS);
+        for _ in 0..NUM_ROWS{
+            col.push(" ");
+        }
+        cols.push(col);
         let mut col = (0..NUM_ROWS).map(|_| " ").collect::<Vec<&str>>();
         frame.push(col);
     } */
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::error::Error;
-use std::time::Duration;
 use crossterm::event::{Event, KeyCode};
+use invaders::frame::{self, new_frame};
+use invaders::render::render;
 use rusty_audio::Audio;
 use rusty_timer::Timer;
 use crossterm::{terminal, event};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::cursor::{Show, Hide};
 use crossterm::ExecutableCommand;
-use std::io;
+use crossbeam::{channel};
+use std::{io, thread, time::Duration};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -26,8 +28,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = channel::unbounded();
+    let render_thread = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout= io::stdout();
+        render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let current_frame = match render_rx.recv() {
+                Ok(returned_frame) => returned_frame,
+                // For when the channel is closed ↓
+                Err(_) => break,
+            };
+            render(&mut stdout, &last_frame, &current_frame, false);
+            last_frame = current_frame;
+        }
+    });
+
     // Game loop
     'gameloop: loop {
+        let current_frame = new_frame();
         // Handle input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
@@ -40,10 +60,19 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        // Draw & render
+        // It'll crash the first few times since receiver is not set up: discard result ↓
+        let _ = render_tx.send(current_frame);
+        // Game loop will be much faster than render loop, introduce artificial delay such that we don't try to render too many frames per second
+        thread::sleep(Duration::from_millis(1));
+
     }
 
 
     //Cleanup
+    drop(render_tx); // This should theoretically be automatically done anyway
+    render_thread.join();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
